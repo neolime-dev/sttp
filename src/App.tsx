3d7b75b25cd11056279732c84865cd504948c3db
MatL <matheuslimaribeiro03@gmail.com>
2025-06-27 14:52:00 -0300

    Add SearchHandler component
    Merged Form and Handle into a single, reusable and optimized component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Clock from "./components/Clock"
-import Handler from "./components/Handler"
-import Form from "./components/Form"
+import SearchHandler from "./components/SearchHandler"
 
 function App() {
     return (
     <div className="App">
       <Clock />
-      <Form />
-      <Handler />
+      <SearchHandler />
     </div>
 
   )
