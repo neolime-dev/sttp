87bf8facb4a00c082edd89e02e37a3406240041b
MatL <matheuslimaribeiro03@gmail.com>
2025-07-01 10:37:22 -0300

    Update Search
    Fixed the problem by doing research using the shortcuts of some sites

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import Clock from "./components/Clock"
-import Handler from "./components/Handler"
-import Form from "./components/Form"
+import SearchHandler from "./components/SearchHandler"
+
 
 function App() {
     return (
     <div className="App">
       <Clock />
-      <Form />
-      <Handler />
+      <SearchHandler />
     </div>
 
   )
